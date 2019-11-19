6f77628be866a514210eccdfbbecd0707dfd4237
umit.gezer <umitgezer2@gmail.com>
2019-11-19 23:55:59 +1100

    clean up

--- src/main/java/gezer/Main.java
+++ src/main/java/gezer/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Welcome to Reverse Polish Calc, please enter input:");
+        System.out.println("Welcome, please enter input:");
         while (scanner.hasNextLine()) {
             String input = scanner.nextLine();
 
